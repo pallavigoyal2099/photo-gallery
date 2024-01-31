c7e4639633ef8e6402f46a463ded501ec51b9af6
Pallavi Goyal <pallavigoyal2099@gmail.com>
2024-01-31 22:10:13 +0530

    Updated code

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,9 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // Defaults to localStorage for web
+import storage from 'redux-persist/lib/storage'; 
 
-import rootReducer from './wishlistReducer'; // Your combined reducer
+import rootReducer from './wishlistReducer'; 
 
 const persistConfig = {
   key: 'root',
