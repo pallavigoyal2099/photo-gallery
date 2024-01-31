0dc4b33a552e2c0f06e5cf8e4f047e2499578266
Pallavi Goyal <pallavigoyal2099@gmail.com>
2024-01-31 23:52:22 +0530

    empty wishlist handled

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -2,31 +2,42 @@ import React from "react";
 import { wishListData } from "../store/wishlistReducer";
 import { useSelector } from "react-redux";
 import ImageComponent from "./ImageComponent";
+import Home from "./Home";
+import { Link } from "react-router-dom";
 
 const Wishlist = () => {
   const wishlist = useSelector(wishListData);
   const reversedData = wishlist.slice().reverse();
   return (
-    <div className="mb-16 w-[60rem] my-12">
+    <div className="mb-16 w-[60rem] my-12 ">
       <div className="flex items-center flex-col pt-8">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl sm:px-6 ">
           My Wishlist
         </h1>
       </div>
-      <div className="grid md:grid-cols-4 gap-4  grid-cols-2 mx-4">
-        {reversedData.map((result) => (
-          <ImageComponent
-            key={result.id}
-            smallHeight={"20rem"}
-            smallWidth={"10rem"}
-            height="0"
-            width="0"
-            imageObj={result}
-            image={result.urls.small}
-            alt={result.alt_description}
-          />
-        ))}
-      </div>
+      {reversedData.length > 0 ? (
+        <div className="grid md:grid-cols-4 gap-4  grid-cols-2 mx-4">
+          {reversedData.map((result) => (
+            <ImageComponent
+              key={result.id}
+              smallHeight={"20rem"}
+              smallWidth={"10rem"}
+              height="0"
+              width="0"
+              imageObj={result}
+              image={result.urls.small}
+              alt={result.alt_description}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="text-center bold text-xl my-16">
+          Oops!!No item in wishlist, Please Like some thing from{" "}
+          <Link className="text-underlined text-blue-700" to={"/"}>
+            here{" "}
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
